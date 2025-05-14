ae448ccc19bc2df7f35dc428b3dddbe56ed0ddc0
Tanmayrajput8 <tanmayrajput1778@gmail.com>
2025-05-14 13:06:06 +0530

    Update settings.py

--- cep vendor/tender_management/tender_management/settings.py
+++ cep vendor/tender_management/tender_management/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(*+9@t3($qr2kwr+q(j^#^tct2q40g$ntwpy84yonicdh&sg_*"
+SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-dev-key")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+# ✅ Allow localhost by default for development
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+
 
 
 # Application definition
